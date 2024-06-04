8adb885647c50a2f5573c601b75796f75c21e41e
Gianni Petrella <gianni.petrella99@gmail.com>
2024-06-04 16:36:58 +0200

    ci: docs passed to deploydocs()

--- docs/make.jl
+++ docs/make.jl
@@ -13,16 +13,13 @@ makedocs(
     sitename = "QuiverTools",
     authors = "Gianni Petrella",
     format = Documenter.HTML(),
-    branch = "docs",
     # format = Documenter.LaTeX(), # builds pdf, does not like the github Documenter action for now. Use only in local build.
     modules = [QuiverTools],
     pages = [   "QuiverTools" => "index.md", 
-                "Tutorial" => "tutorial.md",
-                "All methods" => "methods.md"]
-)
+    "Tutorial" => "tutorial.md",
+    "All methods" => "methods.md"]
+    )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-
-deploydocs(repo = "github.com/quiver-tools/QuiverTools.jl.git")
+deploydocs(
+    branch = "docs",
+    repo = "github.com/quiver-tools/QuiverTools.jl.git")
