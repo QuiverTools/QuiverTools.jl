fd7c2abb97b124960a713fc3f06c7fe1aafee855
Gianni Petrella <gianni.petrella99@gmail.com>
2024-07-06 16:26:00 +0200

    style: lint

--- src/moduli.jl
+++ src/moduli.jl
@@ -1,4 +1,9 @@
-export QuiverModuli, is_nonempty, dimension, Poincaré_polynomial, Betti_numbers, is_smooth
+export QuiverModuli,
+	is_nonempty,
+	dimension,
+	Poincaré_polynomial,
+	Betti_numbers,
+	is_smooth
 
 
 mutable struct QuiverModuli
@@ -26,61 +31,69 @@ mutable struct QuiverModuli
 end
 
 function show(io::IO, M::QuiverModuli)
-    print(io, "Moduli space of $(M.condition) representations of $(M.Q) with dimension vector $(M.d) and stability parameter $(M.theta)")
+	print(
+		io,
+		"Moduli space of $(M.condition) representations of $(M.Q)
+with dimension vector $(M.d) and stability parameter $(M.theta)",
+	)
 end
 
 function is_nonempty(M::QuiverModuli)
-    if M.condition == "stable"
-        return has_stables(M.Q, M.d, M.theta)
-    elseif M.condition == "semistable"
-        return has_semistables(M.Q, M.d, M.theta)
-    end
+	if M.condition == "stable"
+		return has_stables(M.Q, M.d, M.theta)
+	elseif M.condition == "semistable"
+		return has_semistables(M.Q, M.d, M.theta)
+	end
 end
 
 function dimension(M::QuiverModuli)
-    if M.condition == "stable"
-        return 1 - Euler_form(M.Q, M.d, M.d)
-    elseif M.condition == "semistable"
-        if has_semistables(M.Q, M.d, M.theta)
-            return maximum(dimension_of_luna_stratum(tau) for tau in all_luna_types(M.Q, M.d, M.theta))
-        end
-    else
-        return "-∞" # how?
-    end
-    return "-∞"
+	if M.condition == "stable"
+		return 1 - Euler_form(M.Q, M.d, M.d)
+	elseif M.condition == "semistable"
+		if has_semistables(M.Q, M.d, M.theta)
+			return maximum(dimension_of_luna_stratum(tau)
+						   for tau in all_luna_types(M.Q, M.d, M.theta))
+		end
+	else
+		return "-∞" # how?
+	end
+	return "-∞"
 end
 
 """
-Checks if the moduli space is infinitesimally rigid by verifying wether the Teleman quantization criterion of [arXiv:2311.17003](https://doi.org/10.48550/arXiv.2311.17003).
+Checks if the moduli space is infinitesimally rigid by verifying wether
+the Teleman quantization criterion of
+[arXiv:2311.17003](https://doi.org/10.48550/arXiv.2311.17003) holds.
 
-Only a sufficent criterion is implemented, so the function may return "not known" even if the moduli space is rigid.
+Only a sufficent criterion is implemented,
+so the function may return "not known" even if the moduli space is rigid.
 """
 function is_rigid(M::QuiverModuli)
-    if is_acyclic(M.Q)
-        bounds = all_Teleman_bounds(M.Q, M.d, M.theta)
-        weights = all_weights_endomorphisms_universal_bundle(M.Q, M.d, M.theta)
-        if all(weights[hn] < bounds[hn] for hn in keys(bounds))
-            return true
-        end
-    end
-    return "not known"
+	if is_acyclic(M.Q)
+		bounds = all_Teleman_bounds(M.Q, M.d, M.theta)
+		weights = all_weights_endomorphisms_universal_bundle(M.Q, M.d, M.theta)
+		if all(weights[hn] < bounds[hn] for hn in keys(bounds))
+			return true
+		end
+	end
+	return "not known"
 end
 
 
 function dimension_of_luna_stratum(tau)
-    throw(NotImplementedError())
+	throw(NotImplementedError())
 end
 
 function Poincaré_polynomial(M::QuiverModuli)
-    throw(NotImplementedError())
+	throw(NotImplementedError())
 end
 
 function Betti_numbers(M::QuiverModuli)
-    throw(NotImplementedError())
+	throw(NotImplementedError())
 end
 
 function is_smooth(M::QuiverModuli)
-    throw(NotImplementedError())
+	throw(NotImplementedError())
 end
 
 # TODO - dimension of Luna stratum
@@ -96,42 +109,42 @@ end
 
 """Finds a linearization to construct the universal bundles on the moduli space."""
 function linearization(M::QuiverModuli)
-    if gcd(M.d) == 1
-        throw(NotImplementedError())
-    end
-    throw(DomainError("Linearization does not exist as gcd(d) = $(gcd(d)) != 1"))
+	if gcd(M.d) == 1
+		throw(NotImplementedError())
+	end
+	throw(DomainError("Linearization does not exist as gcd(d) = $(gcd(d)) != 1"))
 end
 
 
 """Returns the Chow ring for the given moduli space.
 The optional argument `a` is the linearization used to construct the universal bundles.
 If none is given, one is computed automatically."""
-function Chow_ring(M::QuiverModuli, a::Vector{Int}=linearization(M), standard::Bool=false)
-    return Chow_ring(M.Q, M.d, M.theta, a, standard)
+function Chow_ring(M::QuiverModuli, a::Vector{Int} = linearization(M), standard::Bool = false)
+	return Chow_ring(M.Q, M.d, M.theta, a, standard)
 end
 
 """Returns the point class for the given moduli space.
 """
 function point_class(M::QuiverModuli)
-    return point_class(M.Q, M.d, M.theta)
+	return point_class(M.Q, M.d, M.theta)
 end
 
 """Returns the Todd class for the given moduli space.
 """
 function Todd_class(M::QuiverModuli)
-    return Todd_class(M.Q, M.d, M.theta)
+	return Todd_class(M.Q, M.d, M.theta)
 end
 
 function Hodge_polynomial(M::QuiverModuli)
-    return Hodge_polynomial(M.Q, M.d, M.theta)
+	return Hodge_polynomial(M.Q, M.d, M.theta)
 end
 
 function Picard_rank(M::QuiverModuli)
-    return Picard_rank(M.Q, M.d, M.theta)
+	return Picard_rank(M.Q, M.d, M.theta)
 end
 
 function Hodge_diamond(M::QuiverModuli)
-    return Hodge_diamond(M.Q, M.d, M.theta)
+	return Hodge_diamond(M.Q, M.d, M.theta)
 end
 
 function index(M::QuiverModuli)
