56cf7ce789ac856de04192e55cdbfd0c9cea7a73
Gianni Petrella <gianni.petrella99@gmail.com>
2024-07-17 15:33:59 +0200

    docs: fix remotes in docs

--- docs/make.jl
+++ docs/make.jl
@@ -13,7 +13,6 @@ makedocs(
     sitename = "QuiverTools",
     authors = "Gianni Petrella",
     doctest = false,
-    remotes=nothing,
     modules = [QuiverTools],
     format = Documenter.HTML(),
     # format = Documenter.LaTeX(), # builds pdf, does not like the github Documenter action for now. Use only in local build.
