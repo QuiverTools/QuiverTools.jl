38e630aca0c981534e13b1d877a464eacc9d72ff
Pieter Belmans <pieterbelmans@gmail.com>
2025-01-21 17:37:08 +0100

    build: add a reminder to ourselves

--- docs/docs.jl
+++ docs/docs.jl
@@ -20,6 +20,7 @@ makedocs(;
   modules=[QuiverTools],
   format=Documenter.HTML(),
 
+  # TODO this should detect whether we are working locally or not
   warnonly = true,
 
   # Use the following two parameters for local pdf build.
